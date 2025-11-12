5fb3db77ad75c9c5322e0d7e733e91b2dadee6b8
Efiamotu-1 <musahabeebefy10@gmail.com>
2025-11-12 02:20:50 +0100

    add cors so api calls would work from frontend

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
 
 const userRoute = require('./routes/userRoute');
 const feedbackRoute = require('./routes/feebackRoute');
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://feedbackforge-frontend.vercel.app'
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
+
 app.use(express.json());
+
 
 app.use('/api/v1/users', userRoute)
 app.use('/api/v1/feedbacks', feedbackRoute)
